f7aef665651e9e84d28704c5a5b0ee0b496c0f98
ramielsabahy <ramyelsabahy95@gmail.com>
2018-05-16 22:04:08 +0200

    Updates

---
+++ app/Http/Controllers/ApiAuthController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ApiAuthController extends Controller
+{
+    public function authenticate() {
+		// get user data
+		$credentials = request()->only('email', 'password');
+
+		// check if user credentials are correct
+		try {
+			// using the facade
+			$token = \JWTAuth::attempt($credentials);
+
+			if (!$token) {
+			// unauthorized access
+			return response()->json(['error' => 'invalid_credentials'], 401);
+			}
+		}catch(\Tymon\JWTAuth\Exceptions\JWTException $e) {
+			// internal server error
+			return response()->json(['error' => 'something_went_wrong'], 500);
+		}
+
+		// generate a token
+		// successful
+		return response()->json(['token' => $token], 200);
+	}
+
+	public function register() {
+		// create user
+		$email = request()->email;
+		$name = request()->name;
+		$password = request()->password;
+
+		$user = User::create([
+			'name' => $name,
+			'email' => $email,
+			'password' => bcrypt($password)
+		]);
+
+		// generate token
+		$token = JWTAuth::fromUser($user);
+
+		// return response
+		return response()->json([
+			'token' => $token
+		], 200);
+	}
+
+}
